45f5011a0bc32b7afe06b34609e1685d71c26425
Damilola Alao <oluwadamilola.alao@gmail.com>
2022-02-13 07:25:26 +0100

    first commit

---
+++ my-app/constants/index.js
@@ -0,0 +1,5 @@
+import contract from "../../hardhat-tutorial/artifacts/contracts/Whitelist.sol/Whitelist.json";
+
+export const abi = contract.abi;
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x27CC22fe19b4799CBc3b288031e47Ca3f20Ce63E";
